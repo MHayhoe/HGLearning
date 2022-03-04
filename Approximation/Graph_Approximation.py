6479054f3535eeb51a0fe5f5c6e2087436d76cc4
MHayhoe <m.hayhoe@icloud.com>
2022-03-04 18:09:09 -0500

    Added spectral sparsification code. Issues with size of sparsified graph

---
+++ Approximation/Graph_Approximation.py
@@ -0,0 +1,38 @@
+import numpy as np
+import networkx as nx
+
+
+# Given a graph G (as an adjacency matrix A) and threshold eps, returns a spectral sparsifier H such that
+# (1-eps)L_G <= L_H <= (1+eps)L_G, in the sense of PSD ordering.
+def spectral_sparsifier(A, eps):
+    # Find the incidence matrix B, edge weight matrix W, Laplacian L and its pseudoinverse Lp.
+    H = nx.from_numpy_matrix(A)
+    B = nx.linalg.incidence_matrix(H)
+    W = np.diag([e[-1] for e in H.edges.data("weight", default=1)])
+    L = B @ W @ B.T
+    Lp = np.linalg.pinv(L)
+
+    # Compute effective resistance of the edges
+    R_e = np.diag(B.T @ Lp @ B)
+
+    # Selection probabilities, proportional to edge weights and effective resistances
+    probs = W @ R_e / np.sum(W @ R_e)
+
+    # Parameters for selection of edges
+    c = 1
+    n = len(H.nodes)
+    m = W.shape[0]
+    q = np.ceil(9 * c ** 2 * n / eps ** 2).astype('int')
+
+    # Sample to obtain new edge weights, which are zero if an edge was never selected.
+    rng = np.random.default_rng(0)
+    edge_indices = rng.choice(m, size=q, replace=True, p=probs, axis=1)
+    W_H = np.zeros((m,))
+    W_vals = W / (probs*q)
+
+    for ei in edge_indices:
+        W_H += W_vals[:,ei]
+
+    # Set edge weights accordingly and return the resulting adjacency matrix
+    nx.set_edge_attributes(H, W_H, "weight")
+    return nx.adjacency_matrix(H)
