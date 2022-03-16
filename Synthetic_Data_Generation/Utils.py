9beab3bcbfcec41665d81c1a7089d59eda33d4d6
Mikhail <m.hayhoe@icloud.com>
2022-03-16 10:06:37 -0400

    Added hypergraph diffusions, and plotting

--- Synthetic_Data_Generation/Utils.py
+++ Synthetic_Data_Generation/Utils.py
@@ -1,7 +1,37 @@
 import matplotlib
+import numpy as np
+import hypernetx as hnx
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
+
+
+# Plots the diffusion of the signal x on the hypergraph H, according to L_H
+def plot_diffusion(H, x, num_steps=1):
+    # Plot the hypergraph, and color nodes according to signal values
+    H_draw = hnx.Hypergraph(H.hyperedges)
+    kwargs = {'layout_kwargs': {'seed': 40}, 'with_node_counts': False}
+    color_map = plt.cm.viridis
+    normed = plt.Normalize(x.min(), x.max())
+    plt.subplot(211)
+    hnx.drawing.draw(H_draw, nodes_kwargs={'facecolors': color_map(normed(x)) * (1, 1, 1, 1)}, **kwargs)
+    plt.colorbar(matplotlib.cm.ScalarMappable(norm=normed,cmap=color_map))
+    plt.draw()
+
+    # Perform the diffusion
+    X = np.zeros((num_steps+1,x.shape[0]))
+    X[0,:] = x
+
+    for t in range(1,num_steps+1):
+        X[t,:] = H.diffuse(X[t-1,:])
+
+    # Plot the diffused signals
+    steps = np.tile(np.arange(0,num_steps+1), (H.N, 1)).T
+    plt.subplot(212)
+    plt.plot(steps, X)
+    plt.legend(np.arange(1,H.N+1),loc='upper left')
+    plt.show()
+
 
 def plot_2d_sc(sc):
     fig, ax = plt.subplots()
